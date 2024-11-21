8f94d8e6fa5ce05b9156d76c35153f605bcb8ff6
Maxi Muller <maximuller11@gmail.com>
2024-11-21 09:21:40 -0300

    More CommandsConsole QOL improvements

--- Runtime/CommandsConsole/CommandsConsole.cs
+++ Runtime/CommandsConsole/CommandsConsole.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
@@ -16,6 +17,9 @@ namespace Padoru.Core.DebugConsole
 		private bool showConsole;
 		private string input;
 		private GUIStyle textFieldStyle;
+		private List<string> commandHistory = new();
+		private int historyIndex = -1;
+		private bool moveCursorToEnd;
 
 		public CommandsConsole(CommandsConsoleConfig config)
 		{
@@ -47,9 +51,15 @@ namespace Padoru.Core.DebugConsole
 				input = string.Empty;
 				showConsole = false;
 			}
+			
+			if (Event.current.type == EventType.KeyDown && showConsole)
+			{
+				HandleHistoryNavigation(Event.current.keyCode);
+			}
 
 			DrawConsole();
 			FocusConsole();
+			MoveCursorToEndIfNeeded();
 		}
 
 		private void DrawConsole()
@@ -89,6 +99,7 @@ namespace Padoru.Core.DebugConsole
 			if (showConsole)
 			{
 				input = string.Empty;
+				historyIndex = -1;
 			}
 		}
 
@@ -109,16 +120,79 @@ namespace Padoru.Core.DebugConsole
 				return;
 			}
 
-			var parameters = input.Split(' ');
-			if(parameters.Length <= 0)
+			if (!string.IsNullOrWhiteSpace(input))
 			{
-				return;
-			}
+				if (commandHistory.Count == 0 || commandHistory.Last() != input)
+				{
+					commandHistory.Add(input);
+				}
 
-			var args = parameters.Skip(1).ToArray();
-			if (config.Commands.TryGetValue(parameters[0].ToLower(), out var command))
+				var parameters = input.Split(' ');
+				var args = parameters.Skip(1).ToArray();
+				if (config.Commands.TryGetValue(parameters[0].ToLower(), out var command))
+				{
+					command.Execute(args);
+				}
+			}
+		}
+		
+		private void HandleHistoryNavigation(KeyCode keyCode)
+		{
+			if (keyCode == KeyCode.UpArrow)
 			{
-				command.Execute(args);
+				if (historyIndex == -1)
+				{
+					historyIndex = commandHistory.Count - 1;
+				}
+				else if (historyIndex > 0)
+				{
+					historyIndex--;
+				}
+
+				if (historyIndex >= 0 && historyIndex < commandHistory.Count)
+				{
+					input = commandHistory[historyIndex];
+					SetCursorToEndNextFrame();
+				}
+			}
+			else if (keyCode == KeyCode.DownArrow)
+			{
+				if (historyIndex >= 0)
+				{
+					historyIndex++;
+					if (historyIndex < commandHistory.Count)
+					{
+						input = commandHistory[historyIndex];
+						SetCursorToEndNextFrame();
+					}
+					else
+					{
+						historyIndex = -1;
+						input = string.Empty;
+					}
+				}
+			}
+		}
+		
+		private void SetCursorToEndNextFrame()
+		{
+			moveCursorToEnd = true;
+		}
+
+		private void MoveCursorToEndIfNeeded()
+		{
+			if (moveCursorToEnd)
+			{
+				moveCursorToEnd = false;
+
+				// Force the cursor to the end of the input
+				GUI.FocusControl(TEXT_FIELD_CONTROL_NAME);
+				var textEditor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
+				if (textEditor != null)
+				{
+					textEditor.cursorIndex = input.Length;
+					textEditor.selectIndex = input.Length;
+				}
 			}
 		}
 	}
